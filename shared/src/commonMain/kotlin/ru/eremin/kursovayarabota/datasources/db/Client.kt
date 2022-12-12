5692139252343617888ed5a92f3f8f53e2b36f43
danil <danilka.eremin@gmail.com>
2022-12-12 12:25:46 +0800

    ''''''эээ?''''''

--- shared/src/commonMain/kotlin/ru/eremin/kursovayarabota/datasources/db/Client.kt
+++ shared/src/commonMain/kotlin/ru/eremin/kursovayarabota/datasources/db/Client.kt
@@ -4,28 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Client(
-    val cost: String,
-    val costOrder: String,
     val idClient: String,
-    val idMaster: String,
-    val idModel: String,
-    val idOrder: String,
-    val lastnameClient: String,
-    val lastnameMaster: String,
+    val lastname: String,
     val mail: String,
-    val nameCake: String,
-    val nameClient: String,
-    val nameMaster: String,
-    val patch: String,
+    val name: String,
     val phone: String,
-    val prepayment: String,
-    val presumptiveDate: String,
-    val productionTime: String,
-    val registrationOrder: String,
-    val salary: String,
-    val statusOrder: String,
-    val surnameClient: String,
-    val surnameMaster: String,
-    val type: String,
-    val weight: String
+    val surname: String
 )
--- shared/src/commonMain/kotlin/ru/eremin/kursovayarabota/datasources/db/MasterOrders.kt
+++ shared/src/commonMain/kotlin/ru/eremin/kursovayarabota/datasources/db/MasterOrders.kt
@@ -2,7 +2,6 @@ package ru.eremin.kursovayarabota.datasources.db
 
 import kotlinx.serialization.Serializable
 
-
 @Serializable
 data class MasterOrders(
     val cost: String,
@@ -11,17 +10,22 @@ data class MasterOrders(
     val idMaster: String,
     val idModel: String,
     val idOrder: String,
-    val lastname: String,
-    val name: String,
+    val lastnameClient: String,
+    val lastnameMaster: String,
+    val mail: String,
+    val nameCake: String,
+    val nameClient: String,
+    val nameMaster: String,
     val patch: String,
+    val phone: String,
     val prepayment: String,
     val presumptiveDate: String,
     val productionTime: String,
-    val name_: String,
     val registrationOrder: String,
     val salary: String,
     val statusOrder: String,
-    val surname: String,
+    val surnameClient: String,
+    val surnameMaster: String,
     val type: String,
     val weight: String
 )
